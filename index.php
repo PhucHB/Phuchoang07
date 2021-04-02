968785ec0c39afebeda3c2468959501bd07adfe7
PhucHB <81226356+PhucHB@users.noreply.github.com>
2021-04-02 17:11:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
     <title>Document</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/3.6.95/css/materialdesignicons.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="icon.css">
 </head>
 <body>
       <h1>PPcycle</h1>
@@ -62,5 +65,147 @@
      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+     <hr>
+     <ul class="trangchu">
+        <h2>About us</h2>
+        <p>We are a retail unit of imported vehicles and provide high quality ancillary equipment to consumers.
+         Our unit was established on 7/7/2020
+         Address at:</p>
+         <p>Map</p> <br>
+         <div>
+            <div class="container">
+               <div><h1>Contact Us</h1></div>
+               <br />
+               <div class="row">
+                   <div class="col-md-6">
+                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3833.834036683917!2d108.22842141478559!3d16.07409978887798!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31421825f1045e4f%3A0xcc796e42a4979a!2zNjU4IE5nw7QgUXV54buBbiwgQW4gSOG6o2kgQuG6r2MsIFPGoW4gVHLDoCwgxJDDoCBO4bq1bmcgNTUwMDAwLCBWaWV0bmFt!5e0!3m2!1sen!2sus!4v1617354830212!5m2!1sen!2sus" width="500" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                       
+                   </div>
+                   <br />
+                   <div class="col-md-6">
+                       <form class="my-form">
+                           <div class="form-group">
+                               <label for="form-name">Name</label>
+                               <input type="email" class="form-control" id="form-name" placeholder="Name">
+                           </div>
+                           <div class="form-group">
+                               <label for="form-email">Email Address</label>
+                               <input type="email" class="form-control" id="form-email" placeholder="Email Address">
+                           </div>
+                           <div class="form-group">
+                               <label for="form-subject">Telephone</label>
+                               <input type="text" class="form-control" id="form-subject" placeholder="Subject">
+                           </div>
+                           <div class="form-group">
+                               <label for="form-message">Email your Message</label>
+                               <textarea class="form-control" id="form-message" placeholder="Message"></textarea>
+                           </div>
+                           <button class="btn btn-default" type="submit">Contact Us</button>                
+                       </form>
+                   </div>
+               </div>
+           </div>
+           
+           <style>
+               .my-form {
+                   color: #305896;
+               }
+               .my-form .btn-default {
+                   background-color: #305896;
+                   color: #fff;
+                   border-radius: 0;
+               }
+               .my-form .btn-default:hover {
+                   background-color: #4498C6;
+                   color: #fff;
+               }
+               .my-form .form-control {
+                   border-radius: 0;
+               }
+           </style>
+
+         </div>
+                
+     </ul>
+     <hr>
+     <ul class="trangchu">
+        <h2>San pham noi bac</h2>
+        <section>
+         <article>
+            <h1>YZF R1</h1>
+            <p>R1 2020 hàng nhập khẩu chính hãng</p>
+            <img src="https://dd5394a0b8ca8e97ba29-abf76f3d91a2125517d6c7c409f095c7.ssl.cf1.rackcdn.com/content/common/Models/2021/d7542f7e-9e4c-44f8-80bb-4c08ea2862b0.png" alt="photo" width="100%" height="300px" />
+            <div class="price"> 720,000,000 vnd </div>
+            <ul>
+               <li>Hỗ trả góp 0%</li>
+               <li>Bảo hành 3 năm hoặc 30,000 km</li>
+            </ul>
+            <button>Xem chi tiết</button> <br>
+            <button>Đặt mua</button>
+         </article>
+         <article>
+          <h1>EXCITER 155</h1>
+          <p>EXCITER 155 VVA 2020 hàng nhập khẩu chính hãng</p>
+          <img src="https://yamaha-motor.com.vn/wp/wp-content/uploads/2020/12/Exciter-155-GP-004.png" alt="photo" width="100%" height="300px"/>
+          <div class="price"> 50,000,000 VND </div>
+          <ul>
+             <li>Hỗ trả góp 0%</li>
+             <li>Bảo hành 3 năm hoặc 30,000 km</li>
+          </ul>
+          <button>Xem chi tiết</button> <br>
+          <button>Đặt mua</button>
+       </article>
+       <article>
+          <h1>YZF R3 </h1>
+          <p>R3 2020 hàng nhập khẩu chính hãng</p>
+          <img src="https://yamaha-motor.com.vn/wp/wp-content/uploads/2017/05/2020_YZF320-A_DPBMC_USA_360_026.png" alt="photo" width="100%" height="300px"/>
+          <div class="price"> 129,000,000 VND </div>
+          <ul>
+             <li>Hỗ trả góp 0%</li>
+             <li>Bảo hành 3 năm hoặc 30,000 km</li>
+          </ul>
+          <button>Xem chi tiết</button> <br>
+          <button>Đặt mua</button>
+       </article>
+      </section>
+     </ul>
+     <ul>
+        <div class="NEWS">
+         <h2>NEWS</h2>
+         <section>
+            <article >
+               <h1>khuyen mai nhan dip 30/4-1/5</h1>
+               <img src="http://nhaccuthanhthuy.com/wp-content/uploads/2018/10/giam-10.jpg" alt="photo" width="100%" height="150px"   />
+               <a href="#">nhan xem chi tiet</a>
+            </article>
+ 
+         </section>
+ 
+        </div>
+     </ul>
+     <ul class="ending">
+        <p> Design by PHH gcd0807A</p>
+        <hr>
+        <article class="1">
+         <h3>can mua xe</h3>
+         <a href="#">tin bán xe mơi</a> <br>
+         <a href="#">xe cũ</a> <br>
+         <a href="#">trả góp</a>
+      </article>
+      <article class="1">
+         <h3> can ban xe</h3>
+         <a href="#">định giá</a> <br>
+         <a href="#">đăng kí thời gian bán</a>
+      </article>
+        <hr>
+        <ul class="icon">
+         <div>
+            <li><a href="https://www.facebook.com/profile.php?id=100041460853445" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="facebook" data-abc="true"><i class="mdi mdi-facebook feather icon-facebook facebook" aria-hidden="true"></i></a></li>
+         <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="twitter" data-abc="true"><i class="mdi mdi-twitter feather icon-twitter twitter" aria-hidden="true"></i></a></li>
+         <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="instagram" data-abc="true"><i class="mdi mdi-instagram feather icon-instagram instagram" aria-hidden="true"></i></a></li>
+         </div>
+         
+     </ul>
+     </ul>
 </body>
 </html>
